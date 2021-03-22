1ffbfb97484c6af2d3c90921050f87d212900900
Eduardo Franco <eduardofranco0529@gmail.com>
2021-03-22 16:40:31 -0300

    Foi inserido as páginas: ocorrencia_template.php e frete_template.php eu foi alterado o arquivo api.css

---
+++ frete_template.php
@@ -0,0 +1,26 @@
+<?php include "header.php"; ?>
+
+	<div class="inner-page-intro">
+		<h4>Rastreamento de remesas</h4>
+	</div>
+
+	<img src="images/api/banner_rastreamento.jpg" width="1000" height="350" class="img-responsive" />
+
+
+	<div style="width:100%; padding: 35px; text-align:center">
+    	<h2 style="font-family: 'Montserrat', Arial; color: #d82029; font-weight: 600">RESULTADO DA BUSCA</h2>
+			<hr width="100%">
+			<ul class="resultado_frete">
+				<li><span class="frete_li">ID: </span>0001</li>
+				<li><span class="frete_li">CRIADO EM: </span> 00/00/0000</li>
+				<li><span class="frete_li">ATUALIZADO EM: </span>00/00/0000</li>
+				<li><span class="frete_li">LINK DO ANEXO: </span><a href="#" style="list-style-type: none;">CLIQUE AQUI PARA BAIXAR O PDF</a></li>
+				<li><span class="frete_li">ID DO FRETE: </span>0001</li>
+				<li><span class="frete_li">NÚMEOR DO CTE: </span>0001</li>
+				<li><span class="frete_li">CHAVE DO CTE: </span>0001</li>
+				<li><span class="frete_li">NÚMERO DA NOTA: </span>0001</li>
+</ul>
+<hr width="100%"> 
+	</div>
+
+<?php include "footer.php"; ?>
---
+++ ocorrencia_template.php
@@ -0,0 +1,38 @@
+<?php include "header.php"; ?>
+
+	<div class="inner-page-intro">
+		<h4>Rastreamento de remesas</h4>
+	</div>
+
+	<img src="images/api/banner_rastreamento.jpg" width="1000" height="350" class="img-responsive" />
+
+
+	<div style="width:100%; padding: 35px; text-align:center">
+    	<h2 style="font-family: 'Montserrat', Arial; color: #d82029; font-weight: 600">RESULTADO DA BUSCA</h2>
+			<hr width="100%">
+            <h2 style="font-family: Helvetica, Arial, sans-serif; text-align:left; padding-top: 20px; margin-bottom: 5px">OCORRÊNCIA</h2>
+			<ul class="resultado_frete">
+				<li><span class="frete_li">RECEBIDO POR: </span> RUBRICA</li>
+				<li><span class="frete_li">NÚMERO DO DOCUMENTO: </span></li>
+				<li><span class="frete_li">COMENTÁRIO: </span> </li>
+				<li><span class="frete_li">DATA DA OCORRÊNCIA: </span>2020-12-21T09:43:00.000-03:00</li>
+				<li><span class="frete_li">CRIADO EM: </span>2020-12-22T09:44:01.307-03:00</li>
+				<li><span class="frete_li">ATUALIZADO EM: </span>2020-12-22T09:44:01.307-03:00</li>
+                <li><span class="frete_li">NÚMERO DA NOTA: </span>7586</li>
+                <li><span class="frete_li">TIPO DA NOTA: </span>Invoice::Nfe</li>
+
+            </ul>
+            
+            <h2 style="font-family: Helvetica, Arial, sans-serif; text-align:left; padding-top: 20px; margin-bottom: 5px">FRETE</h2>
+			<ul class="resultado_frete">
+				<li><span class="frete_li">NÚMERO DO CTE: </span> 517</li>
+				<li><span class="frete_li">CHAVE DO CTE: </span>35201204142031000138570920000005171993813159</li>
+				<li><span class="frete_li">PREVISÃO DE ENTREGA: </span>abc</li>
+				<li><span class="frete_li">STATUS </span>finish</li>
+				<li><span class="frete_li">DESCRIÇÃO </span>Entrega/Coleta Realizada Normalmente</li>
+
+            </ul>
+<hr width="100%"> 
+	</div>
+
+<?php include "footer.php"; ?>
